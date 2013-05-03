205687d53f9e65065b8e90282134904249ea8fbd
PC <PC@PC-HP>
2013-05-03 13:45:29 -0300

    comiit segundo

---
+++ main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: PC
+ *
+ * Created on 3 de Maio de 2013, 09:39
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+    cout<<"alteracao"<<endl;
+     cout<<"alteracao"<<endl;
+    cout<<"alteracaoxxxxx"<<endl;
+    printf("Alguma coisa");
+    return 0;
+}
+
